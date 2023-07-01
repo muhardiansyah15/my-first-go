46eb906d457e06b3b3cc3f0e71c885e5bbc4446e
muhardiansyah <muhardiansyah97@gmail.com>
2023-07-02 00:01:45 +0700

    Upgrade to v2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/muhardiansyah15/my-first-go
 
 go 1.18
 
-require github.com/muhardiansyah15/go-say-hello v1.0.2 // indirect
+require github.com/muhardiansyah15/go-say-hello/v4 v4.0.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	go_say_hello "github.com/muhardiansyah15/go-say-hello"
+	go_say_hello "github.com/muhardiansyah15/go-say-hello/v4"
 )
 
 func main() {
-	fmt.Println(go_say_hello.SayHello())
+	fmt.Println(go_say_hello.SayHello("Muhardiansyah"))
 }
